6382e031dc1e226475ceb8ee7357d545267a9397
Alejandro <alexnachovc12@gmail.com>
2025-01-28 23:54:12 -0600

    output types, comments. escape sequences

---
+++ Output/Output.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstC_.Output
+{
+    internal class Output
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hey!"); // doesnt add a new line after the string displayed
+            Console.WriteLine("Hello!"); // adds a new line after displaying String literal
+
+            /*
+             * multi
+             * line
+             * comment
+            */
+
+            Console.WriteLine("\tAlexNachoVC");
+
+            Console.ReadKey();
+        }
+    }
+}
