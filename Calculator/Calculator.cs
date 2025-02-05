2ccea804b2be8206c29735686560fa77fe62a400
Alejandro <alexnachovc12@gmail.com>
2025-02-04 23:59:50 -0600

    calculator program

---
+++ Calculator/Calculator.cs
@@ -0,0 +1,50 @@
+﻿namespace Calculator
+{
+    public class CalculatorClass
+    {
+        public static void Explore()
+        {
+            double num1 = 0;
+            double num2 = 0;
+            double result = 0;
+            char option;
+
+            Console.WriteLine("------------------");
+            Console.WriteLine("Calculator Program");
+            Console.WriteLine("------------------");
+
+            Console.WriteLine("Enter number 1: ");
+            num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter number 2: ");
+            num2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Choose an option: ");
+            Console.WriteLine("+ : Add");
+            Console.WriteLine("- : Subtract");
+            Console.WriteLine("* : Multiply");
+            Console.WriteLine("/ : Divide");
+            option = Convert.ToChar(Console.ReadLine());
+
+            switch (option)
+            {
+                case '+':
+                    Console.WriteLine("Result = " + (num1 + num2) );
+                    break;
+                case '-':
+                    Console.WriteLine("Result = " + (num1 - num2));
+                    break;
+                case '*':
+                    Console.WriteLine("Result = " + (num1 * num2));
+                    break;
+                case '/':
+                    Console.WriteLine("Result = " + (num1 / num2));
+                    break;
+                default:
+                    Console.WriteLine("Invalid option");
+                    break;
+            }
+
+        }
+    }
+}
