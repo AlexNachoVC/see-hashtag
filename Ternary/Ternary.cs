a14e0b3407c01920694ec5198f905cd0f412c1b7
Alejandro <alexnachovc12@gmail.com>
2025-02-08 22:55:52 -0600

    Conditional operator. ternary operator, but in c# its called conditional operator

---
+++ Ternary/Ternary.cs
@@ -0,0 +1,14 @@
+﻿namespace ConditionalOperator
+{
+    public class ConditionalOperatorClass
+    {
+        public static void Explore()
+        {
+            double temperature = 20;
+            string message;
+
+            message = (temperature >= 15) ? "It's warm outside!" : "It's cold outside!";
+            Console.WriteLine(message);
+        }
+    }
+}
